61d87b2fba5f6cf36b422157ca9a002b2f85984f
RizwanAbdul <shaikrizwan289@gmail.com>
2019-06-30 02:13:15 -0400

    delcaring variable types

--- basics.py
+++ basics.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 #
 
-def main():
-    print ("Hello World!")
+class Test:
+    def main():
+      print ("Hello World!")
     a = 1 
     b = 1.2 
     c = "string value"
     d = [1,2,3,4]
-    e=33
-    f="this is rizwan'
-    
-    print(a)
-    print(b)
-    print(c)
-    print(d)
-    print(e)
-    print(f)
-
-if __name__ == "__main__":
-    main()
+    e=True
+    f="this is rizwan" 
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
+    print(type(f))
