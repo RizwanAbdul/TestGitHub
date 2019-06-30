0e5daa393a2f84c94ac7ef95596a71ec2f186e5e
RizwanAbdul <shaikrizwan289@gmail.com>
2019-06-30 01:54:26 -0400

    variable f added

---
+++ basics.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+
+def main():
+    print ("Hello World!")
+    a = 1 
+    b = 1.2 
+    c = "string value"
+    d = [1,2,3,4]
+    e=33
+    f="this is rizwan'
+    
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+
+if __name__ == "__main__":
+    main()
