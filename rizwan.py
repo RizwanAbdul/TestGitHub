f1d942b77e0ec1cc490e5586773ec13d1c63a871
RizwanAbdul <shaikrizwan289@gmail.com>
2019-06-30 23:26:56 -0400

    rizwan added

---
+++ rizwan.py
@@ -0,0 +1,11 @@
+class Test:
+    def __init__(self,name,age,addr):
+        self.name=name
+        self.age=age
+
+    def display(self):
+        print(''My name is:',self.name)
+        print('My age is:',self.age)
+
+
+              
